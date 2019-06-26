fabede147287c5e19662a3e74564b907a855af73
Kimyeongjin92 <dudwlsrla21@naver.com>
2019-06-26 12:48:24 +0900

    Statistics

--- Section07 여러모집단의 평균비교 검정.R
+++ Section07 여러모집단의 평균비교 검정.R
@@ -1,73 +1,58 @@
 ## ===================================================================================
 ## [빅데이터를 활용한 스마트 데이터 전문가 양성과정]  ================================
 ## [제대로 알고 쓰는 통계분석 R]                      ================================
-## 2019-06-26(수) / 여러 모집단의 평균 비교 검정 262page  ============================
+## 2019-06-26(수) / 모집단이 두개인 경우              ================================
 ## ===================================================================================
 
 
 ### ================================================================================
-###  0. 데이터 프레임 다루기 (243Page) ============================================
+###  1. 서로 독립인 두 집단 (262Page) =============================================
 ### ================================================================================
 
-data <- read.table("http://www.amstat.org/publications/jse/datasets/babyboom.dat.txt",header=F)
+data <- read.table("chapter7.txt",header=T)
+boy  <- subset(data,gender==1)
+girl <- subset(data,gender==2)
 
-# 1) 구조확인하기
-nrow(data)
-ncol(data)
-dim(data)
-str(data)      # int(숫자형)
-summary(data)  # 요약통계
-head(data,5)
-tail(data,5)
-table(data$gender)
+var.test(data$weight~data$gender)
 
-# 2) 변수명 확인 및 변경
-names(data)
-row.names(data)
-col.names(data)
-names(data)    <- c("time","gender","weight","minutes")
-names(data)[1] <- "time.24Hrs"
+# 1) 정규성 테스트
+shapiro.test(girl$weight) # P>0.05 정규성을 이룬다.
+qqnorm(girl$weight)       # 정규성 = 직선근처에 점들이 모여있다
+qqline(girl$weight)
 
-# 3) 열 추출하기.
-g1 <- data$time.24Hrs
-g2 <- data$gender
-g3 <- data$weight
-g4 <- data$minutes
+shapiro.test(boy$weight)  # P<0.01798 정규성 NONO
+qqnorm(boy$weight)      
+qqline(boy$weight)
 
-# 4) 행 추출하기.
-data[data$gender==2 & data$weight > mean(data$weight), ]
-subset(data,gender==2 & data$weight > mean(data$weight))
+iriss <- subset(iris, Species=='setosa')
+shapiro.test(iriss$Sepal.Length) # P>0.05, 정규성 있음
+qqnorm(iriss$Sepal.Length)
+qqline(iriss$Sepal.Length)
 
-# 5) 행열 추출하기.
-data[data$gender==2 & data$weight > mean(data$weight), c(2,4)]
-
-# 6) 데이터 정제하기
-data$minutes <- ifelse(data$minutes >= 1000, NA, data$minutes)
-
-# 7) 데이터 프레임 저장하기.
-chapter7 <- data[,c(2,3)]
-write.table(chapter7, "chapter7.txt")
-write.table(chapter7, "chapter7(2).txt",row.names=F) # 행번호 제거
-
-# 8) 데이터 프레임 불러오기.
-write.csv("data.txt",header=T,na.string=c('99'))     # 99에 해당하는 데이터를 NA처리
-
-# 9) 결측 판별 함수(is.na)
-is.na(c(1, NA, 3, NA, 5)) # NA = TURE로 표시된다.
-nonNA.sum <- sum(data$minutes[!is.na(data$minutes)])
-
-# 10) factor형 자료 처리
-data$gender<-factor(data$gender)
-summary(data)
-str(data)
-
-# 11) summaryBy를 이용한 통계
-install.packages("doBy")
-library(doBy)
-summaryBy(weight~gender, data=data, FUN=c(mean, sd), na.rm=T)
-# 통계를 구할 변수 ~ 집단을 구분할 변수, 데이터, FUN=수식(length, mean, sd 등)
+shapiro.test(iriss$Petal.Width) # P 굉장히 작다. 정규성 없음
+qqnorm(iriss$Petal.Width)
+qqline(iriss$Petal.Width)
 
 
-### ================================================================================
-###  1. 모집단이 두개(서로 독립인 두 집단, 서로 대응인 두 집단) (262Page) =========
-### ================================================================================
+# 2) 등분산성 ( 그래프가 홀쭉한지 펑퍼짐한건지) 
+# 분산을 비교하기 위해서 F-test를 실시. 
+# var.test(y축 ~ x축)
+var.test(data$weight ~ data$gender) 
+# P>0.05 두개가 같다. 그러나 0.07의 숫자인만큼 언뜻 보기에는 달라 보일 수 있다.
+
+
+# 3) 평균이 같은지 다른지 알아본다.
+# ex) H0 두개가 같다 
+#     H1 다르다(한쪽 검정 : 혹은 크거나 작다)
+#    Two Sample T
+
+t.test(data$weight ~ data$gender,
+       mu=0, 
+       alternative="less", 
+       var.equal=T)            # 분산이 같다는 가정하에서.
+# P(0.067)>0.05 같다.
+# 그러나 남자가 정규성을 띄지 않기 때문에 정확한 결론은 아니다. 
+
+
+
+
